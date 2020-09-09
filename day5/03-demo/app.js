62adfde4654f8bcada9b8707dacb3bc85a385eba
louisevi <15757113726@163.com>
2020-09-09 17:28:41 +0800

    '完成留言板demo'

--- day5/03-demo/app.js
+++ day5/03-demo/app.js
@@ -10,9 +10,47 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 设置全局变量，运用于服务器的整个生命周期
+let entries = [];
+app.locals.entries = entries;
+
+// 设置用户表单提交数据的接收中间件，所有提交的信息都会保留在req。body中
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.get('/', (req, res)=>{
    res.render('index');
 });
+
+app.get('/new', (req, res)=>{
+    res.render('new');
+});
+
+// 数据提交
+app.post('/new', (req, res)=>{
+    // console.log(req.body);
+    if(!req.body.title || !req.body.content){
+        res.status(400).send('留言必须要有标题和内容');
+        return ;
+    }
+
+    // 保存用户的留言数据
+    entries.push({
+        title: req.body.title,
+        content: req.body.content,
+        published: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+    });
+
+    console.log(entries);
+
+    // 回到主界面，重定向
+    res.redirect('/')
+
+})
+
+// 走到最后都没匹配上，走404页面
+app.use((req, res)=>{
+    res.status(404).render('404');
+})
 
 app.listen(3000, ()=>{
     console.log('服务器已启动');
